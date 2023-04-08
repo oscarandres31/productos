ba1c7f37b0f25b8747c14599fc98862f13c86b59
oscarandres31 <oarrieta3@gmail.com>
2023-04-08 15:08:48 -0500

    modificando toml 6

--- app/controllers/concerns/language.rb
+++ app/controllers/concerns/language.rb
@@ -7,10 +7,10 @@ module Language
     private
 
     def switch_locale(&action)
-      I18n.with_locale(locale_form_header, &action)
+      I18n.with_locale(locale_from_header, &action)
     end
 
-    def locale_form_header
+    def locale_from_header
       request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
     end
   end
